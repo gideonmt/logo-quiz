0c958cb788fa08e654badfc6825e10587ca096f7
gideon <gmarcustrask26@d-e.org>
2023-11-16 09:27:01 -0500

    vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,12 @@
-import adapter from '@sveltejs/adapter-static';
-
-export default {
-    kit: {
-        // Your preferred adapter, such as adapter-static or adapter-node.
-        // See SvelteKit documentation for more details on adapters.
-        adapter: adapter({
-            // Options for the adapter if needed.
-            // For example, you can specify the output directory.
-            // output: 'build'
-        }),
-        // Other configuration options for your project
-    },
+import adapter from '@sveltejs/adapter-vercel';
+ 
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+    adapter: adapter({
+      runtime: 'nodejs18.x',
+    }),
+  },
 };
+ 
+export default config;
